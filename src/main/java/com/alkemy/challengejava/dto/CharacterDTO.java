27a8c9c90ddfe9146bdb545ddec123cd23ae4cb4
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2022-08-04 17:53:35 -0300

    Agrego la DTO Basicos

--- src/main/java/com/alkemy/challengejava/dto/CharacterDTO.java
+++ src/main/java/com/alkemy/challengejava/dto/CharacterDTO.java
@@ -8,20 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 
-public class CharacterDTO {
-    private Long id;
-
-    private String image;
-
-    private String name;
-
-    private int age;
-
-    private int weight;
-
-    private String history;
-
+public class CharacterDTO extends CharacterBasicDTO {
     private Set<MovieDTO> movies;
-
     private boolean deleted;
 }
